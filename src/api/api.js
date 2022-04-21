5fbaf76d6f9c61ad0167f9cbb26fe2aee26ae485
Роман Кривошеев <krivosheev@amz.lan>
2022-04-21 15:47:17 +0300

    add follow, unfollow, axios-create, DAL

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL:'https://social-network.samuraijs.com/api/1.0/',  //автоматически будет приклеиваться к строке адреса
+    headers: {
+        "API-KEY": "1ff674f1-079c-4d68-a126-7a2284f39d5d"
+      }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance
+          .get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+    }
+}
+
+// export const getUsers = (currentPage = 1, pageSize = 10) => {
+//     return instance
+//       .get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+// }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from './Users.module.css'
 import usersPhoto from '../../assets/images/users.jpg'
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
 
@@ -12,72 +13,86 @@ let Users = (props) => {
       pages.push(i)
     }
 
-  return <div>
-    {
-      <div>
+  return (
+    <div>
+      {
         <div>
-          {pages.map((p) => {
-            // className={this.props.currentPage === p ? s.selectedPage : ""} - исключает ошибку с классом
-            return (
-              <span
-                className={props.currentPage === p && styles.selectedPage}
-                onClick={(e) => {
-                  props.onPageChanged(p);
-                }}
-              >
-                {p}
-              </span>
-            );
-          })}
-        </div>
-        {props.users.map((u) => (
-          <div key={u.id}>
-            <span>
-              <div>
-                <NavLink to={'/profile/' + u.id}>
-                  <img
-                    src={u.photos.small != null ? u.photos.small : usersPhoto}
-                    className={styles.usersPhoto}
-                    alt=""
-                    srcset=""
-                  />
-                </NavLink>
-              </div>
-              <div>
-                {u.followed ? (
-                  <button
-                    onClick={() => {
-                      props.unfollow(u.id);
-                    }}
-                  >
-                    Unfollow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      props.follow(u.id);
-                    }}
-                  >
-                    Follow
-                  </button>
-                )}
-              </div>
-            </span>
-            <span>
-              <span>
-                <div>{u.fullname}</div>
-                <div>{u.status}</div>
-              </span>
-              <span>
-                <div>{"u.location.country"}</div>
-                <div>{"u.location.city"}</div>
-              </span>
-            </span>
+          <div>
+            {pages.map((p) => {
+              // className={this.props.currentPage === p ? s.selectedPage : ""} - исключает ошибку с классом
+              return (
+                <span
+                  className={props.currentPage === p && styles.selectedPage}
+                  onClick={(e) => {
+                    props.onPageChanged(p);
+                  }}
+                >
+                  {p}
+                </span>
+              );
+            })}
           </div>
-        ))}
-      </div>
-    }
-  </div>;
+          {props.users.map((u) => (
+            <div key={u.id}>
+              <span>
+                <div>
+                  <NavLink to={"/profile/" + u.id}>
+                    <img
+                      src={u.photos.small != null ? u.photos.small : usersPhoto}
+                      className={styles.usersPhoto}
+                      alt=""
+                      srcset=""
+                    />
+                  </NavLink>
+                </div>
+                <div>
+                  {u.followed 
+                  ?  <button onClick={() => { 
+                    axios
+                      .delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                        withCredentials: true,
+                        headers: {
+                          "API-KEY": "1ff674f1-079c-4d68-a126-7a2284f39d5d"
+                        }
+                      })
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.unFollow(u.id);
+                        }
+                      });
+                  }}>Unfollow</button> 
+                  :  <button onClick={() => {
+                    axios
+                      .post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                        withCredentials: true,
+                        headers: {
+                          "API-KEY": "1ff674f1-079c-4d68-a126-7a2284f39d5d"
+                        }
+                      })
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(u.id);
+                        }
+                      });
+                     }}> Follow </button> }
+                </div>
+              </span>
+              <span>
+                <span>
+                  <div>{u.fullname}</div>
+                  <div>{u.status}</div>
+                </span>
+                <span>
+                  <div>{"u.location.country"}</div>
+                  <div>{"u.location.city"}</div>
+                </span>
+              </span>
+            </div>
+          ))}
+        </div>
+      }
+    </div>
+  );
 };
 
 export default Users
