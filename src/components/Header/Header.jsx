951e8460af2b6e056ec02ad850ba145660d89c83
Роман Кривошеев <krivosheev@amz.lan>
2022-04-21 09:00:25 +0300

    add coockie, login, auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
 
-
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img
         src="https://dsinterior.ru/wp-content/uploads/2021/01/—Pngtree—instagram-color-icon-instagram-logo_3547787.png"
         alt=""
       />
+      <div className={s.loginBlock}>
+        { props.isAuth ? props.login
+        : <NavLink to={'/login'}>Login</NavLink> }
+      </div>
     </header>
   );
 };
