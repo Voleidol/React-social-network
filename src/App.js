28df8cf07c52ca88fd3e51748bdc069ecd8e5e68
Роман Кривошеев <krivosheev@amz.lan>
2022-04-26 15:04:46 +0300

    add redirect profile and dialogs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import LoginPage from "./components/Login/Login";
 
 
 const App = (props) => {
@@ -24,6 +25,7 @@ const App = (props) => {
               <Route path="/settings" />
               <Route path="/friends" />
               <Route path="/users" element={<UsersContainer />} />
+              <Route path="/login" element={<LoginPage />} />
             </Routes>
           </div>
         </div>
